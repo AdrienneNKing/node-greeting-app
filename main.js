9101ebbd54d02fbfcf6a54d628866419705aef93
Adrienne <adriennenking@gmail.com>
2019-06-01 11:16:49 -0700

    Basic Node App

---
+++ main.js
@@ -0,0 +1,10 @@
+const greetings = require("./greeting.js");
+
+greetings.greetingEnglish();
+greetings.greetingJapanese();
+greetings.greetingIcelandic();
+greetings.greetingSpanish();
+
+const tip = require("./tip.js");
+
+tip.calculate(35);
---
+++ tip.js
@@ -0,0 +1,4 @@
+exports.calculate = function(total) {
+  const tip = total * .20
+  console.log(tip);
+}
